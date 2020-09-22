153b73d10da9296093e29a872581b359a72163e0
Rejaulali <rejaulali1999@gmail.com>
2020-09-22 23:01:41 +0530

    working on Responsiveness of NavBar

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,22 @@
-let menuDisplay = true
-function showMenu(){
-    menuDisplay = !menuDisplay
-    let menu = document.getElementById('menu');
-    if(menuDisplay){
-        menu.style.width='0%';
-    }
-    else{
-        
-        menu.style.width='100%';
-    }
+const showMenu = () =>{
+    console.log("this is called")
+    let burger = document.querySelector('.burger');
+    let cross = document.querySelector('.cross');
+    let menu = document.querySelector('.menu-container');
+    let navLinks = document.querySelectorAll('.menu li')
+    burger.addEventListener('click',() =>{
+        menu.classList.toggle('nav-active')
+        burger.style.filter = 'blur(5px)'
+        navLinks.forEach((link,index) =>{
+            link.style.animation = `navLink 0.3s ease forwards ${index/9 + 0.3}s`;
+        })
+    })
+    cross.addEventListener('click',() =>{
+        menu.classList.toggle('nav-active')
+        burger.style.filter = 'blur(0px)'
+        navLinks.forEach((link,index) =>{
+            link.style.animation = ""
+        })
+    })
 }
+showMenu();
