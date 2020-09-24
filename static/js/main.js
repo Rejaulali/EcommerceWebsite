63ce61f246f08da4d1cc726bcc9defbc36ed9c4b
Rejaulali <rejaulali1999@gmail.com>
2020-09-24 11:43:10 +0530

    another update

--- static/js/main.js
+++ static/js/main.js
@@ -4,11 +4,18 @@ const showMenu = () =>{
     let cross = document.querySelector('.cross');
     let menu = document.querySelector('.menu-container');
     let navLinks = document.querySelectorAll('.menu li')
+    let dropdown = document.querySelector('.dropdown')
     burger.addEventListener('click',() =>{
         menu.classList.toggle('nav-active')
         burger.style.filter = 'blur(5px)'
-        navLinks.forEach((link,index) =>{
-            link.style.animation = `navLink 0.3s ease forwards ${index/9 + 0.3}s`;
+        let index = 0;
+        navLinks.forEach((link) =>{
+            listclass= link.className;
+            if(!(listclass == 'dropdownlist'))
+            {
+                link.style.animation = `navLink 0.3s ease forwards ${index/9 + 0.3}s`;
+                index +=1;
+            }
         })
     })
     cross.addEventListener('click',() =>{
@@ -18,5 +25,20 @@ const showMenu = () =>{
             link.style.animation = ""
         })
     })
+    let add = false
+    dropdown.addEventListener('click',() =>{
+       let ul = document.querySelector('.dropdownul');
+       add =!(add)
+       if(add)
+       {
+            ul.classList.add('dropactive')
+        }
+       else{
+        ul.classList.remove('dropactive')
+       }
+       
+       console.log("this is called")
+    })
+    
 }
 showMenu();
