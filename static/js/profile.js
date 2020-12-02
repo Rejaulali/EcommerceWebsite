2bfc0c3d140f124930865a1ab2f49e4116ef583a
rejaulali <rejaulali199@gmail.com>
2020-12-02 21:41:54 +0530

    100% completed

--- static/js/profile.js
+++ static/js/profile.js
@@ -18,3 +18,36 @@ const profile = ()=>{
 	})
 }
 profile()
+
+
+const updateOnlyImage = ()=>{
+	let formContainer = document.querySelector('.update-form2')
+	let updateForm = document.querySelector('#updateForm2')
+	let fullUpdateForm = document.querySelector('#updateForm')
+	let camraButton = document.querySelector('.circular-image')
+	let cancelButton = document.querySelectorAll('.cancelButton2')
+	let submitBtn = document.querySelector('.update-form2 #submitImage')
+	let imageField1 = document.querySelector('#imageField')
+	let imageField2 =document.querySelector('#id_profile_pic')
+	imageField1.files = imageField2.files
+	submitBtn.addEventListener('click', ()=>{
+		event.preventDefault()
+		imageField2.files = imageField1.files
+		fullUpdateForm.submit()
+
+	})
+	camraButton.addEventListener('click',()=>{
+		formContainer.style.zIndex = "3"
+		formContainer.style.opacity="1"
+		updateForm.style.top="inherit"
+	})
+	cancelButton.forEach((item)=>{
+		item.addEventListener('click',()=>{
+			event.preventDefault()
+			formContainer.style.zIndex = "-3"
+			formContainer.style.opacity="0"
+			updateForm.style.top="-100%"		
+		})
+	})
+}
+updateOnlyImage()
