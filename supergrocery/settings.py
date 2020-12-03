d7e8ba533ee764a023d2ec456e6a3071b797744a
rejaulali <rejaulali199@gmail.com>
2020-12-03 14:38:24 +0530

    added static root

--- supergrocery/settings.py
+++ supergrocery/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = /profile/'@spo8&fp!g#02h(8^(dec4$@^q7=yii%l!8)*e-^+3q5vhwopy'
+SECRET_KEY = '@spo8&fp!g#02h(8^(dec4$@^q7=yii%l!8)*e-^+3q5vhwopy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
